cd423614172cc2a8d5172ae9a239b38cdda3e3ec
Karina <eddychirinos.unac@gmail.com>
2022-12-30 19:26:46 -0300

    Dados e script em streamlit

---
+++ streamlit_for_Time_series/readme.md
@@ -0,0 +1,17 @@
+# `Streamlit` para Série Temporal
+
+Neste script vamos implementar uma interfaz simples para a visualização de dados. Esses dados são registros de marcas entre os anos $2000$ e $2017$.
+
+Os dados usados são do portal de [dados.gov.br](https://dados.gov.br/home), estes dados são dados do `Anuário Estatístico de Propriedade Industrial`.
+
+
+## Executando o script
+
+streamlit run streamlit_app.py
+
+
+
+
+
+
+Thanks God!
---
+++ streamlit_for_Time_series/streamlit_app.py
@@ -0,0 +1,30 @@
+'''
+Data Scientist.: Dr.Eddy Giusepe Chirinos Isidro
+'''
+# Link de estudo --> https://github.com/MaiconGavino/inpi_serie_temporal
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_excel("data.xlsx", decimal='.')
+
+def gera_grafico(graf):
+    st.header('Registro de Marca por ano no Estado do '+ graf)
+
+    fig = px.bar(df, x=df['Ano'], y=df[graf],text_auto='.2s')
+    fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+    fig.update_layout(xaxis_title= 'Anos', yaxis_title = '',xaxis_tickangle=-90)
+    fig.update_yaxes(showticklabels=False)
+    st.plotly_chart(fig, use_container_width = False)
+    
+
+def main(): 
+    page = st.sidebar.selectbox('Selecione o Estado', [x for x in df.columns if x in ["Acre","Alagoas","Amazonas","Amapá","Bahia","Ceará",
+                                                                                      "Distrito Federal","Espírito Santo","Goiás","Maranhão", "Minas Gerais",
+                                                                                      "Mato Grosso do Sul","Mato Grosso","Pará","Paraíba","Pernambuco",
+                                                                                      "Piauí","Paraná","Rio de Janeiro","Rio Grande do Norte",
+                                                                                      "Rondônia", "Roraima", "Rio Grande do Sul","Santa Catarina","Sergipe",
+                                                                                      "São Paulo", "Tocantins"]])
+    gera_grafico(page)
+if __name__ == '__main__':
+    main()
